492e1f44ac7c5160f8763a1b20e78379356d1593
dfgdfgertt <dfgdfgertt@github.com>
2020-12-16 21:52:35 +0700

    test commit

---
+++ app/src/main/java/com/example/crudlistview_17030771/SV.java
@@ -0,0 +1,30 @@
+package com.example.crudlistview_17030771;
+
+public class SV {
+    private int id;
+    private String name;
+
+    public SV(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public SV() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
